e0d410c535ad46e232145ddd097b35ea3e83a13c
amireujanian00 <amireujaniann@gmail.com>
2023-06-28 02:24:13 -0300

    operadores lógicos

--- Operadoreslógicos.js
+++ Operadoreslógicos.js
@@ -1,8 +1,37 @@
-function mayorymenorypar(num){
+/* function mayorymenorypar(num){
     if(num > 5 && num < 10 && num % 2 === 0)
     console.log (true);
     else console.log(false);
 }
 
 mayorymenorypar(7);
-mayorymenorypar(8);
+mayorymenorypar(8); */
+
+function operadorOr (str) {
+    if (str === "Amir" || str.length < 2) 
+    console.log(true);
+    else console.log(false);
+};
+
+operadorOr ("A");
+
+function negación (permiso) {
+    if (permiso !== true)
+    console.log ("Tiene permiso");
+};
+
+negación (true);
+negación (false);
+
+function condicionCompleja (num) {
+    if (num > 9 && num % 2 === 0 || num ===3)
+    console.log(true);
+    else console.log (false);
+};
+
+condicionCompleja(10);
+condicionCompleja(6);
+condicionCompleja(3);
+condicionCompleja(5);
+
+
