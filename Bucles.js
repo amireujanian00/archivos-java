5ddbf24f29f076601efb6b4d6ee16f53392d2d9c
amireujanian00 <amireujaniann@gmail.com>
2023-06-28 17:51:39 -0300

    Bucles

---
+++ Bucles.js
@@ -0,0 +1,27 @@
+/* var suma =  0;
+
+suma = suma + 1;
+suma = suma + 2;
+suma = suma + 3;
+suma = suma + 4;
+
+console.log (suma); */
+
+var suma = 0;
+
+for (var i = 0 ; i < 5 ; i++ ) {
+    suma = suma + i;
+    console.log ("variable de iteración:", i);
+}
+
+console.log ("Variable Suma:", suma);
+
+var A = 0;
+
+while (A < 3) {
+    A = A +1;
+    console.log(A);
+};
+
+
+
