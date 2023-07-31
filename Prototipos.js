e368531bc62b45f7c18e3ef52b33f83717f53ff6
amireujanian00 <amireujaniann@gmail.com>
2023-07-31 16:13:12 -0300

    Archivos Java

--- Prototipos.js
+++ Prototipos.js
@@ -1,19 +1,19 @@
-// Array.prototype.mayoresQueTres = function () {
-//   var arregloModificado = [];
+Array.prototype.mayoresQueTres = function () {
+  var arregloModificado = [];
 
-//   for (let i = 0; i < this.length; i++) {
-//     if (this[i] > 3) {
-//       arregloModificado.push(false);
-//     } else {
-//       arregloModificado.push(this[i]);
-//     }
-//   }
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] > 3) {
+      arregloModificado.push(false);
+    } else {
+      arregloModificado.push(this[i]);
+    }
+  }
 
-//   return arregloModificado;
-// };
+  return arregloModificado;
+};
 
-// var arreglo = [1, 2, 3, 4, 5];
-// var nuevoArreglo = arreglo.mayoresQueTres();
+var arreglo = [1, 2, 3, 4, 5];
+var nuevoArreglo = arreglo.mayoresQueTres();
 
 // console.log(nuevoArreglo);
 
